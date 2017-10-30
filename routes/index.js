f57063775a895fc39edd230b778f92a345b49ce4
Dan Mercer <danielmercer1@gmail.com>
2017-10-30 13:45:32 -0600

    Added browser tagging to uniquely identify users.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+const idgen = require('human-readable-ids').hri;
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chip and Dan\'s CP 4' });
+  res.render('index', {
+    title: 'Chip and Dan\'s CP 4',
+    newDeviceID: idgen.random(),
+  });
 });
 
 module.exports = router;
