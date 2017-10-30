0d6256567f8ed57419aa9d7a2f157298e00dd20d
Dan Mercer <danielmercer1@gmail.com>
2017-10-30 13:02:51 -0600

    Change title in routes/index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chip and Dan\'s CP 4' });
 });
 
 module.exports = router;
