51a9b14acdd6528e3d6b2c40a78930f31e8f71a3
Dan Mercer <danielmercer1@gmail.com>
2017-10-31 20:34:58 -0600

    Removed copy-pasta in play.js

--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -3,20 +3,5 @@ angular.module('app', [])
 
 function playCtrl($scope, GameApi) {
   const api = new GameApi(cp4_global_uid);
-	$scope.username = cp4_global_uid;
-  $scope.games = [];
-
-  refreshGames();
-
-  $scope.createGame = () => api.createGame()
-    .then(refreshGames)
-
-  $scope.startGame = id => api.startGame(id)
-    .then(() => api.listGames())
-    .then(games => $scope.games = games);
-
-  function refreshGames() {
-    return api.listGames()
-      .then(games => $scope.games = games);
-  }
+	
 }
