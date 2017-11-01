9c3d01e2ff92579cd30f782d41c0e1e40a446789
Dan Mercer <danielmercer1@gmail.com>
2017-10-31 21:39:26 -0600

    Added connection-error handling to poller in play state

--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -5,12 +5,23 @@ function playCtrl($scope, GameApi) {
   const api = new GameApi(cp4_global_uid);
 
   $scope.game = cp4_game;
+  $scope.connectionErr = false;
 
   // Refresh game every few hundred milliseconds
   setInterval(refreshGame, 900);
 
   function refreshGame() {
     api.getGame($scope.game.id)
-      .then(game => $scope.game = game);
+      .then(game => $scope.game = game)
+      .then(hideErr)
+      .catch(showErr);
+  }
+
+  function showErr() {
+    $scope.connectionErr = true;
+  }
+
+  function hideErr() {
+    $scope.connectionErr = false;
   }
 }
