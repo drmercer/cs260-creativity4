81b246faaf58e87ccf3c65ea0e663ef171cad691
Dan Mercer <danielmercer1@gmail.com>
2017-10-31 21:06:36 -0600

    Added code to refresh the current game in the play state

--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -3,5 +3,14 @@ angular.module('app', [])
 
 function playCtrl($scope, GameApi) {
   const api = new GameApi(cp4_global_uid);
-	
+
+  $scope.game = cp4_game;
+
+  // Refresh game every few hundred milliseconds
+  setInterval(refreshGame, 900);
+
+  function refreshGame() {
+    api.getGame($scope.game.id)
+      .then(game => $scope.game = game);
+  }
 }
